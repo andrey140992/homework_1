7c1dcc12e860b21d99c6d6fd0cc009a1f0a3c979
Kovalev Andrey <kovalev1000000andrei@yandex.ru>
2021-09-12 17:19:37 +0300

    commit #3 12.09.21

--- allReports.rb
+++ allReports.rb
@@ -1,6 +1,7 @@
 require 'csv'
 
 class AllReports
+
     attr_reader :vms_csv, :prices_csv, :volumes_csv
 
     def initialize(vms_csv, prices_csv, volumes_csv)
@@ -9,112 +10,88 @@ class AllReports
         @volumes_csv = volumes_csv
     end
 
-    def highest_price
+    def parser
+        @@vms_table = CSV.parse(File.read(@vms_csv), headers: %i[id cpu ram hdd_type hdd_capacity])
+        @@vms_prices =  CSV.parse(File.read(@prices_csv), headers: %i[type price])
+        @@volumes_table = CSV.parse(File.read(@volumes_csv), headers: %i[id hdd_type hdd_capacity])
+    end
 
-        vms_table = CSV.parse(File.read(@vms_csv), headers: %i[id cpu ram hdd_type hdd_capacity])
-        vms_prices =  CSV.parse(File.read(@prices_csv), headers: %i[type price])
-        volumes_table = CSV.parse(File.read(@volumes_csv), headers: %i[id hdd_type hdd_capacity])
+
+    def prices_vm
+
+        parser()
 
         vms_sums = {}
 
-        vms_table.each do |id, cpu, ram, hdd_type, hdd_capacity|
+        @@vms_table.each do |id, cpu, ram, hdd_type, hdd_capacity|
 
-            vm_sum = cpu[1].to_i * vms_prices[:price][0].to_i + ram[1].to_i * vms_prices[:price][1].to_i
+            vm_sum = cpu[1].to_i * @@vms_prices[:price][0].to_i + ram[1].to_i * @@vms_prices[:price][1].to_i
         
-                if hdd_type[1] == vms_prices[:type][2]
-                    vm_sum +=  vms_prices[:price][2].to_i
-                elsif hdd_type[1] == vms_prices[:type][3]
-                    vm_sum += vms_prices[:price][3].to_i 
-                elsif hdd_type[1] == vms_prices[:type][4] 
-                    vm_sum += vms_prices[:price][4].to_i
+                if hdd_type[1] == @@vms_prices[:type][2]
+                    vm_sum +=  @@vms_prices[:price][2].to_i
+                elsif hdd_type[1] == @@vms_prices[:type][3]
+                    vm_sum += @@vms_prices[:price][3].to_i 
+                elsif hdd_type[1] == @@vms_prices[:type][4] 
+                    vm_sum += @@vms_prices[:price][4].to_i
                 end
         
              vms_sums[id[1]] = vm_sum
         
         end
 
-        volumes_table.each do |id, hdd_type, hdd_capacity|
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
             dop = 0
          
-                if hdd_type[1] == vms_prices[:type][2]
-                    dop += vms_prices[:price][2].to_i 
-                elsif hdd_type[1] == vms_prices[:type][3]
-                    dop += vms_prices[:price][3].to_i
-                elsif hdd_type[1] == vms_prices[:type][4]
-                    dop += vms_prices[:price][4].to_i
+                if hdd_type[1] == @@vms_prices[:type][2]
+                    dop += @@vms_prices[:price][2].to_i 
+                elsif hdd_type[1] == @@vms_prices[:type][3]
+                    dop += @@vms_prices[:price][3].to_i
+                elsif hdd_type[1] == @@vms_prices[:type][4]
+                    dop += @@vms_prices[:price][4].to_i
                 end
          
             vms_sums[id[1]] += dop
             
         end
 
-        expensivest_vm = vms_sums.to_a.sort_by(&:last)
-
-        puts " Самая дорогая ВМ #{expensivest_vm[-1]}"
-
+        @@prices_vm = vms_sums.to_a.sort_by(&:last)
 
     end
+
+   
+    def highest_price
+
+        prices_vm()
+
+        puts " Самая дорогая ВМ #{@@prices_vm[-1]}"
+
+    end
+
 
     def lowest_price
 
-        vms_table = CSV.parse(File.read(@vms_csv), headers: %i[id cpu ram hdd_type hdd_capacity])
-        vms_prices =  CSV.parse(File.read(@prices_csv), headers: %i[type price])
-        volumes_table = CSV.parse(File.read(@volumes_csv), headers: %i[id hdd_type hdd_capacity])
+        prices_vm()
+      
+        puts " Самая дешевая ВМ #{@@prices_vm[0]}"
 
-        vms_sums = {}
-
-        vms_table.each do |id, cpu, ram, hdd_type, hdd_capacity|
-
-            vm_sum = cpu[1].to_i * vms_prices[:price][0].to_i + ram[1].to_i * vms_prices[:price][1].to_i
-        
-                if hdd_type[1] == vms_prices[:type][2]
-                    vm_sum +=  vms_prices[:price][2].to_i
-                elsif hdd_type[1] == vms_prices[:type][3]
-                    vm_sum += vms_prices[:price][3].to_i 
-                elsif hdd_type[1] == vms_prices[:type][4] 
-                    vm_sum += vms_prices[:price][4].to_i
-                end
-        
-             vms_sums[id[1]] = vm_sum
-        
-        end
-
-        volumes_table.each do |id, hdd_type, hdd_capacity|
-            dop = 0
-         
-                if hdd_type[1] == vms_prices[:type][2]
-                    dop += vms_prices[:price][2].to_i 
-                elsif hdd_type[1] == vms_prices[:type][3]
-                    dop += vms_prices[:price][3].to_i
-                elsif hdd_type[1] == vms_prices[:type][4]
-                    dop += vms_prices[:price][4].to_i
-                end
-         
-            vms_sums[id[1]] += dop
-            
-        end
-
-        expensivest_vm = vms_sums.to_a.sort_by(&:last)
-
-        puts " Самая дешевая ВМ #{expensivest_vm[0]}"
     end
+
 
     def largest_VM_by_type
 
-        vms_table = CSV.parse(File.read(@vms_csv), headers: %i[id cpu ram hdd_type hdd_capacity])
-        volumes_table = CSV.parse(File.read(@volumes_csv), headers: %i[id hdd_type hdd_capacity])
+        parser()
 
         vms_all_types = {}
         vms_types = {}
         vms_types_length ={}
 
-        vms_table.each do |id, cpu, ram, hdd_type, hdd_capacity|
+        @@vms_table.each do |id, cpu, ram, hdd_type, hdd_capacity|
   
             vms_all_types[id[1]] = cpu[1].to_i + ram[1].to_i
              
         end
 
-        volumes_table.each do |id, hdd_type, hdd_capacity|
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
 
             vms_types[id[1]] = vms_types[id[1]] || []
           
@@ -138,25 +115,24 @@ class AllReports
 
         puts " Самая объемная ВМ по параметру type id: #{vms_all_types_sort[-1][0].to_s + ", количество подключенных типов: " + vms_all_types_sort[-1][1].to_s}"
 
-
     end
+
 
     def VM_with_maximum_number_of_dop_disks
 
-        volumes_table = CSV.parse(File.read(@volumes_csv), headers: %i[id hdd_type hdd_capacity])
+        parser()
 
-        
         vms_types = {}
         vms_types_length ={}
 
-        volumes_table.each do |id, hdd_type, hdd_capacity|
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
 
             vms_types[id[1]] = vms_types[id[1]] || []
           
             vms_types[id[1]] ? vms_types[id[1]] << hdd_type[1] : vms_types[id[1]] = 0
             
         end
-
+       
         vms_types.each do |id, types|
   
             vms_types_length[id] = vms_types[id].length
@@ -168,14 +144,101 @@ class AllReports
 
     end
 
+    def VM_with_maximum_number_of_dop_disks_ssd
+
+        parser()
+
+        vms_ssd ={}
+        vms_ssd_length = {}
+
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
+            
+            vms_ssd[id[1]] = vms_ssd[id[1]] || []
+
+            if hdd_type[1] == "ssd"
+                vms_ssd[id[1]] << hdd_type[1]
+            end
+          
+           
+        end
+
+        vms_ssd.each do |id, types|
+  
+            vms_ssd_length[id] = vms_ssd[id].length
+        
+        end
+
+        vms_ssd_length_sort = vms_ssd_length.to_a.sort_by(&:last)
+        puts " ВМ у которой подключено больше всего дополнительных дисков по типу ssd (по количеству штук) id: #{vms_ssd_length_sort[-1][0].to_s  + ", количество подключенных дополнительных дисков ssd: " + vms_ssd_length_sort[-1][1].to_s}"
+
+    end
+
+    def VM_with_maximum_number_of_dop_disks_sas
+
+        parser()
+
+        vms_sas ={}
+        vms_sas_length = {}
+
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
+            
+            vms_sas[id[1]] = vms_sas[id[1]] || []
+
+            if hdd_type[1] == "sas"
+                vms_sas[id[1]] << hdd_type[1]
+            end
+          
+           
+        end
+
+        vms_sas.each do |id, types|
+  
+            vms_sas_length[id] = vms_sas[id].length
+        
+        end
+
+        vms_sas_length_sort = vms_sas_length.to_a.sort_by(&:last)
+        puts " ВМ у которой подключено больше всего дополнительных дисков по типу sas (по количеству штук) id: #{vms_sas_length_sort[-1][0].to_s  + ", количество подключенных дополнительных дисков sas: " + vms_sas_length_sort[-1][1].to_s}"
+
+    end
+
+    def VM_with_maximum_number_of_dop_disks_sata
+
+        parser()
+
+        vms_sata ={}
+        vms_sata_length = {}
+
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
+            
+            vms_sata[id[1]] = vms_sata[id[1]] || []
+
+            if hdd_type[1] == "sata"
+                vms_sata[id[1]] << hdd_type[1]
+            end
+          
+           
+        end
+
+        vms_sata.each do |id, types|
+  
+            vms_sata_length[id] = vms_sata[id].length
+        
+        end
+
+        vms_sata_length_sort = vms_sata_length.to_a.sort_by(&:last)
+        puts " ВМ у которой подключено больше всего дополнительных дисков по типу sata (по количеству штук) id: #{vms_sata_length_sort[-1][0].to_s  + ", количество подключенных дополнительных дисков sata: " + vms_sata_length_sort[-1][1].to_s}"
+
+    end
+
+    
     def VM_with_maximum_vol_of_dop_disks
 
-        volumes_table = CSV.parse(File.read(@volumes_csv), headers: %i[id hdd_type hdd_capacity])
+        parser()
 
-        vms_types = {}
         vms_types_vol = {}
 
-        volumes_table.each do |id, hdd_type, hdd_capacity|
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
 
             vms_types_vol[id[1]] = vms_types_vol[id[1]] || 0
           
@@ -187,6 +250,75 @@ class AllReports
 
         vms_types_vol_sort = vms_types_vol.to_a.sort_by(&:last)
         puts " ВМ у которой подключено больше всего дополнительных дисков (по объему) id: #{vms_types_vol_sort[-1][0].to_s  + ", объем памяти: " + vms_types_vol_sort[-1][1].to_s}"
+
+    end
+
+    def VM_with_maximum_vol_of_dop_disks_ssd
+
+        parser()
+
+        vms_ssd_vol = {}
+
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
+            
+            vms_ssd_vol[id[1]] = vms_ssd_vol[id[1]] || 0
+
+            if hdd_type[1] == "ssd"
+                vms_ssd_vol[id[1]] += hdd_capacity[1].to_i
+            end
+          
+           
+        end
+
+        
+        vms_ssd_vol_sort = vms_ssd_vol.to_a.sort_by(&:last)
+        puts " ВМ у которой подключено больше всего дополнительных дисков ssd (по объему) id: #{vms_ssd_vol_sort[-1][0].to_s  + ", объем памяти: " + vms_ssd_vol_sort[-1][1].to_s}"
+
+    end
+
+    def VM_with_maximum_vol_of_dop_disks_sas
+
+        parser()
+
+        vms_sas_vol = {}
+
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
+            
+            vms_sas_vol[id[1]] = vms_sas_vol[id[1]] || 0
+
+            if hdd_type[1] == "sas"
+                vms_sas_vol[id[1]] += hdd_capacity[1].to_i
+            end
+          
+           
+        end
+
+        
+        vms_sas_vol_sort = vms_sas_vol.to_a.sort_by(&:last)
+        puts " ВМ у которой подключено больше всего дополнительных дисков sas (по объему) id: #{vms_sas_vol_sort[-1][0].to_s  + ", объем памяти: " + vms_sas_vol_sort[-1][1].to_s}"
+
+    end
+
+    def VM_with_maximum_vol_of_dop_disks_sata
+
+        parser()
+
+        vms_sata_vol = {}
+
+        @@volumes_table.each do |id, hdd_type, hdd_capacity|
+            
+            vms_sata_vol[id[1]] = vms_sata_vol[id[1]] || 0
+
+            if hdd_type[1] == "sata"
+                vms_sata_vol[id[1]] += hdd_capacity[1].to_i
+            end
+          
+           
+        end
+
+        
+        vms_sata_vol_sort = vms_sata_vol.to_a.sort_by(&:last)
+        puts " ВМ у которой подключено больше всего дополнительных дисков sata (по объему) id: #{vms_sata_vol_sort[-1][0].to_s  + ", объем памяти: " + vms_sata_vol_sort[-1][1].to_s}"
 
     end
 
--- reports.rb
+++ reports.rb
@@ -1,9 +1,68 @@
 require "./allReports"
 
-report = AllReports.new("vms.csv", "prices.csv", "volumes.csv")
+report = AllReports.new("./csv/vms.csv", "./csv/prices.csv", "./csv/volumes.csv")
 
-report.highest_price
-report.lowest_price
-report.largest_VM_by_type
-report.VM_with_maximum_number_of_dop_disks
-report.VM_with_maximum_vol_of_dop_disks
+
+
+puts "Выберете отчет:
+1.Самая дорогая ВМ 
+2.Самая дешевая ВМ
+3.Самая объемная ВМ по параметру type
+4.ВМ у которой подключено больше всего дополнительных дисков (по количеству штук)
+5.ВМ у которой подключено больше всего дополнительных дисков (по объему)"
+
+choice = gets.chomp
+
+if choice == "1"
+    report.highest_price 
+elsif choice == "2"
+    report.lowest_price  
+elsif choice == "3"
+    report.largest_VM_by_type 
+elsif choice == "4"
+    puts " 
+    Выберете тип жесткого диска:
+    1)  ssd
+    2)  sas
+    3)  sata"
+
+    choice = gets.chomp
+    
+    if choice == "1"
+        report.VM_with_maximum_number_of_dop_disks_ssd
+    elsif choice == "2"
+        report.VM_with_maximum_number_of_dop_disks_sas
+    elsif choice == "3"
+        report.VM_with_maximum_number_of_dop_disks_sata
+    else 
+        puts "Такого отчета нет!"
+    end 
+
+elsif choice == "5"
+    puts " 
+    Выберете тип жесткого диска:
+    1)  ssd
+    2)  sas
+    3)  sata"
+
+    choice = gets.chomp
+
+    if choice == "1"
+        report.VM_with_maximum_vol_of_dop_disks_ssd
+    elsif choice == "2"
+        report.VM_with_maximum_vol_of_dop_disks_sas
+    elsif choice == "3"
+        report.VM_with_maximum_vol_of_dop_disks_sata
+    else 
+        puts "Такого отчета нет!"
+    end 
+     
+else
+    puts"Такого отчета нет!"
+end
+
+#report.highest_price
+#report.lowest_price
+#report.largest_VM_by_type
+#report.VM_with_maximum_number_of_dop_disks
+#report.VM_with_maximum_vol_of_dop_disks
