155d75da856ee0a09e846b7eb8af7ba4db7c3a24
Kovalev Andrey <kovalev1000000andrei@yandex.ru>
2021-09-12 17:29:09 +0300

    commit #4 12.09.21

--- reports.rb
+++ reports.rb
@@ -8,17 +8,22 @@ puts "Выберете отчет:
 1.Самая дорогая ВМ 
 2.Самая дешевая ВМ
 3.Самая объемная ВМ по параметру type
-4.ВМ у которой подключено больше всего дополнительных дисков (по количеству штук)
-5.ВМ у которой подключено больше всего дополнительных дисков (по объему)"
+4.ВМ у которой подключено больше всего дополнительных дисков с учетом типа диска(по количеству штук)
+5.ВМ у которой подключено больше всего дополнительных дисков с учетом типа диска (по объему)
+6.ВМ у которой подключено больше всего дополнительных дисков без учета типа диска (по количеству штук)
+7.ВМ у которой подключено больше всего дополнительных дисков без учета типа диска (по объему)"
 
 choice = gets.chomp
 
 if choice == "1"
-    report.highest_price 
+    report.highest_price
+
 elsif choice == "2"
     report.lowest_price  
+
 elsif choice == "3"
     report.largest_VM_by_type 
+
 elsif choice == "4"
     puts " 
     Выберете тип жесткого диска:
@@ -56,13 +61,15 @@ elsif choice == "5"
     else 
         puts "Такого отчета нет!"
     end 
+
+elsif choice == "6"
+    report.VM_with_maximum_number_of_dop_disks
+
+elsif choice == "7"
+    report.VM_with_maximum_vol_of_dop_disks
      
 else
     puts"Такого отчета нет!"
 end
 
-#report.highest_price
-#report.lowest_price
-#report.largest_VM_by_type
-#report.VM_with_maximum_number_of_dop_disks
-#report.VM_with_maximum_vol_of_dop_disks
+
